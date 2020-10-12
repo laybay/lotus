bd0fad365c697f48d7ab436140cf11eb3cedf7a6
Steven Allen <steven@stebalien.com>
2020-10-12 00:07:00 -0700

    [WIP] set heights to force-test tape upgrade

--- build/params_testnet.go
+++ build/params_testnet.go
@@ -20,17 +20,17 @@ var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	UpgradeSmokeHeight: DrandMainnet,
 }
 
-const UpgradeBreezeHeight = 41280
+const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 
-const UpgradeSmokeHeight = 51000
+const UpgradeSmokeHeight = -2
 
-const UpgradeIgnitionHeight = 94000
-const UpgradeRefuelHeight = 130800
+const UpgradeIgnitionHeight = -3
+const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(138720)
+var UpgradeActorsV2Height = abi.ChainEpoch(0)
 
-const UpgradeTapeHeight = 140760
+const UpgradeTapeHeight = 1
 
 // This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
 // Miners, clients, developers, custodians all need time to prepare.
